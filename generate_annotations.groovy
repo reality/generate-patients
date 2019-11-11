b459671b7800fda93a298a42355515d3d09c17c1
reality <tinmachin3@gmail.com>
2019-11-11 20:26:37 +0000

    worx

--- generate_annotations.groovy
+++ generate_annotations.groovy
@@ -10,17 +10,35 @@ import ontologizer.ontology.*
 import hpo.ItemId.ItemDatabase;
 import groovy.json.*
 
-// Parameters 
-def ontologyFile = './hp.obo'
-def annotationFile = './phenotype_annotation.tab'
-def id = "114030";
+// setup ...
+def O_FILE = './hp.obo'
+def A_FILE = './phenotype_annotation.tab'
+
+def dCount = Integer.parseInt(args[0]) // number of diseases
+def pCount = Integer.parseInt(args[1]) // number of patients per disease
+
+def rnd = new Random()
+
+// Get IDs
+def ids = new File(A_FILE).text.split('\n').collect { if(it.indexOf('OMIM') != -1) { it.split('\t')[1] } }
+ids.removeAll([null])
+println ids
 
 // Generate patients
-def simulator = new AnnotationSimulator(ontologyFile, annotationFile, OntologyProjectType.HPO);
-def patients = simulator.simulatePatients(ItemDatabase.OMIM, id, 1000, 0.2, 0.4, 2, 10);
+def simulator = new AnnotationSimulator(O_FILE, A_FILE, OntologyProjectType.HPO);
+
+results = []
+(1..dCount).each { 
+  def idx = rnd.nextInt(ids.size())
+  def id = ids[idx]
+
+  results += simulator.simulatePatients(ItemDatabase.OMIM, id, pCount, 0.2, 0.4, 2, 10);
+}
+
+results = results.flatten()
 
 // Output
-def json = JsonOutput.toJson(patients)
-new File('patients.json').write(json)
+def json = JsonOutput.toJson(results)
+new File('results.json').write(json)
 
-println 'done'
+println 'Done'
